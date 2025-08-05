bc7fad3aac274e0df725d6acc7aadcfd890bbeb5
Shan Ali Shah Sayed <shanalishah7@hotmail.com>
2025-08-05 17:15:31 -0400

    Update writingassistant.py

--- writingassistant.py
+++ writingassistant.py
@@ -2,7 +2,8 @@
 
 import streamlit as st
 # from langchain.chat_models import ChatOpenAI
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 import os
 
